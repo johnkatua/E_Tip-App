c6a4446d0324e468621de6d4e0e89f382a54f5ec
john katua <johnkatua99@gmail.com>
2021-03-14 12:29:17 +0300

    added a functionality to toggle the sidebar

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,21 +2,40 @@ import React, {useState} from 'react';
 import '../Dashboard/Dashboard.css';
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
 import { IconContext } from 'react-icons/lib';
+import { useSpring, animated } from 'react-spring';
 
 function Dashboard() {
   const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => {
+    setSidebar(!sidebar);
+  }
+
+  const SideBarNavStyle = useSpring({
+    left: sidebar ? '0' : '-100%'
+  });
+
+  const MoveSidebar = useSpring({
+    left: sidebar ? '100%' : '0' 
+  });
+
   return (
     <IconContext.Provider value={{color: "#fff"}}>
-      <div className='Nav'>
+      <animated.div className='Nav' style={MoveSidebar}>
         <Link to='#' className='NavIcon'>
-          <FaIcons.FaBars />
+          <FaIcons.FaBars onClick={showSidebar}/>
         </Link>
         <h1 className='NavHeader'>E-Tip Banking</h1>
-      </div>
-      <nav className='SideBarNav' sidebar={sidebar}>
-        <div className='SideBarWrap'></div>
-      </nav>
+      </animated.div>
+      <animated.nav className='SideBarNav' sidebar={sidebar} style={SideBarNavStyle}>
+        <div className='SideBarWrap'>
+          <Link to='#' className='NavIcon'>
+            <AiIcons.AiOutlineClose onClick={showSidebar} />
+          </Link>
+        </div>
+      </animated.nav>
     </IconContext.Provider>
   )
 }
